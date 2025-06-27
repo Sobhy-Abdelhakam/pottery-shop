e845642edf3de6146b7d0597d6212b92527283b1
sayed_younis <elsayedseif17@gmail.com>
2025-06-28 01:55:41 +0300

    edit main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pottery/congifures/light_theme.dart';
+import 'package:pottery/congifures/dark_theme.dart';
+import 'package:pottery/feature/landing/views/landing_pages_view.dart';
+import 'package:pottery/feature/landing/bloc/landing_cubit.dart';
 
 void main() {
   runApp(
-     const MyApp()
+    BlocProvider<LandingCubit>(
+      create: (context) => LandingCubit(),
+      child: const MyApp(),
+    ),
   );
 }
 
@@ -11,7 +19,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Flutter Landing Pages',
+      theme: lightTheme, // Light theme
+      darkTheme: darkTheme, // Dark theme
+      themeMode: ThemeMode.light,
+      home: const LandingPagesView(),
       debugShowCheckedModeBanner: false,
     );
   }
