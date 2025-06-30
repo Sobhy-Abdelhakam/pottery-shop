73d21e612b975497f555145f2a58c20017b136dd
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-06-30 16:19:06 +0300

    add models

---
+++ lib/feature/checkout/models/payment_method.dart
@@ -0,0 +1,26 @@
+
+abstract class PaymentMethod {
+  String get name;
+  String get icon;
+}
+
+class VisaPayment extends PaymentMethod {
+  @override
+  String get name => 'Visa';
+  @override
+  String get icon => 'assets/icons/visa.png';
+}
+
+class VodafoneCashPayment extends PaymentMethod {
+  @override
+  String get name => 'Vodafone Cash';
+  @override
+  String get icon => 'assets/icons/digital-wallet.png';
+}
+
+class CashOnDeliveryPayment extends PaymentMethod {
+  @override
+  String get name => 'Cash on Delivery';
+  @override
+  String get icon => 'assets/icons/cash.png';
+}
