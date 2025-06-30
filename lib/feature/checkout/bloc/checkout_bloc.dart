723bce70332c37babbb5700e6fcc4b7d1b254f27
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-07-01 00:05:22 +0300

    Refactore checkout bloc

--- lib/feature/checkout/bloc/checkout_bloc.dart
+++ lib/feature/checkout/bloc/checkout_bloc.dart
@@ -1,15 +1,30 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pottery/feature/checkout/bloc/checkout_event.dart';
 import 'package:pottery/feature/checkout/bloc/checkout_state.dart';
+import 'package:pottery/feature/checkout/models/delivery_option.dart';
 
 class CheckoutBloc extends Bloc<CheckoutEvent, CheckoutState> {
-  CheckoutBloc() : super(CheckoutState()) {
+  CheckoutBloc(double initialSubtotal)
+      : super(CheckoutState(
+          subtotal: initialSubtotal,
+          deliveryFee: 10.0, // Default to home delivery fee
+          total: initialSubtotal + 10.0,
+        )) {
     on<SelectPaymentMethod>((event, emit) {
       emit(state.copyWith(selectedPaymentMethod: event.paymentMethod));
     });
 
     on<SelectDeliveryOption>((event, emit) {
-      emit(state.copyWith(selectedDeliveryOption: event.deliveryOption));
+      double newDeliveryFee = 0.0;
+      if (event.deliveryOption == DeliveryOption.homeDelivery) {
+        newDeliveryFee = 10.0; // Example home delivery fee
+      }
+      final newTotal = state.subtotal + newDeliveryFee;
+      emit(state.copyWith(
+        selectedDeliveryOption: event.deliveryOption,
+        deliveryFee: newDeliveryFee,
+        total: newTotal,
+      ));
     });
 
     on<SubmitOrder>((event, emit) async {
